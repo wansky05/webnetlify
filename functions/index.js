c06c4565db4aeaf012ccaf9bfd3ddae714b5e938
Rurizora <bottoramku@gmail.com>
2025-03-09 12:11:07 +0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -18,7 +18,7 @@ const router = express.Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 router.get('/', (req, res) => {
-    res.sendFile('/.netlify/functions/index/home.html');
+    res.sendFile('./functions/home.html');
 });
 
 router.get('/ytget/:id', async (req,res) => {
