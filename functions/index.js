8f0e9eaa0e42b9ed644158179f1c9165884aeb9a
Rurizora <bottoramku@gmail.com>
2025-03-09 11:27:10 +0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,27 @@
 // YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 const express = require("express");
 const serverless = require("serverless-http");
-
+const yta = require('./yta.js')
 const app = express();
 const router = express.Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
+
+router.get('/yta/:id', async (req,res) => {
+const q = req.query.data
+const emulate = () =>{
+async function start() {
+try{
+let baseUrl = `${req.protocol}://${req.headers.host}`
+const respon = await yta(q,baseUrl)
+  res.send(respon)
+}catch(e){
+    res.end()
+}
+}
+start()
+}
+emulate()
+});
 
 app.use("/.netlify/functions/index", router);
 
