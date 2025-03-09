8fde3046be0bff1d9c5d7cf09a3d7a1833f7136d
Rurizora <bottoramku@gmail.com>
2025-03-09 12:20:12 +0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -18,7 +18,7 @@ const router = express.Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 router.get('/', (req, res) => {
-    res.sendFile(__dirname);
+    res.send(__dirname);
 });
 
 router.get('/ytget/:id', async (req,res) => {
